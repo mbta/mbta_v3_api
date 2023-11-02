dae2dec4089a0aa1893858390ca7fb0a7d442498
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2023-11-02 16:04:51 -0400

    feat: Lower minimum Elixir version to 1.14

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule V3Api.MixProject do
     [
       app: :v3_api,
       version: "0.0.1",
-      elixir: "~> 1.15",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
