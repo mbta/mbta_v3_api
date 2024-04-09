8f98eacba0f8c0a7754c9ba54a910f3070a00abe
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2024-04-09 15:15:33 -0400

    feat: Trips.by_route_and_directions

--- lib/mbta_v3_api/trips.ex
+++ lib/mbta_v3_api/trips.ex
@@ -2,6 +2,9 @@ defmodule MBTAV3API.Trips do
   @moduledoc """
   Responsible for fetching Trip data from the MBTA V3 API.
   """
+  alias MBTAV3API.Routes.Route
+  alias MBTAV3API.Schedules.Trip
+  alias MBTAV3API.Trips.Parser
 
   def by_id(id, opts \\ []) do
     {get_json_fn, opts} = Keyword.pop(opts, :get_json_fn, &MBTAV3API.get_json/2)
@@ -9,8 +12,34 @@ defmodule MBTAV3API.Trips do
   end
 
   def by_route(route_id, opts \\ []) do
+    opts
+    |> route_option(route_id)
+    |> get_trips()
+  end
+
+  @spec by_route_and_directions(Route.id_t(), [Trip.direction_id_t()]) :: [Trip.t()]
+  @spec by_route_and_directions(Route.id_t(), [Trip.direction_id_t()], keyword()) :: [Trip.t()]
+  def by_route_and_directions(route_id, directions, opts \\ []) do
+    opts
+    |> route_option(route_id)
+    |> direction_option(directions)
+    |> get_trips()
+    |> Map.get(:data, [])
+    |> Enum.map(&Parser.parse/1)
+  end
+
+  @spec route_option(keyword(), Route.id_t()) :: keyword()
+  defp route_option(opts, route_id), do: put_in(opts[:route], route_id)
+
+  @spec direction_option(keyword(), [Trip.direction_id_t()]) :: keyword()
+  defp direction_option(opts, [single_direction_id | []]),
+    do: put_in(opts[:direction_id], single_direction_id)
+
+  defp direction_option(opts, _both_directions), do: opts
+
+  @spec get_trips(keyword()) :: JsonApi.t() | {:error, any}
+  defp get_trips(opts) do
     {get_json_fn, opts} = Keyword.pop(opts, :get_json_fn, &MBTAV3API.get_json/2)
-    opts = put_in(opts[:route], route_id)
     get_json_fn.("/trips/", opts)
   end
 end
--- test/mbta_v3_api/trips_test.exs
+++ test/mbta_v3_api/trips_test.exs
@@ -3,6 +3,7 @@ defmodule MBTAV3API.TripsTest do
   use ExUnit.Case
 
   alias JsonApi.Item
+  alias MBTAV3API.Schedules.Trip
   alias MBTAV3API.Trips
 
   describe "by_id" do
@@ -23,6 +24,60 @@ defmodule MBTAV3API.TripsTest do
       opts = [get_json_fn: fn "/trips/", [route: ^route_id] -> response end]
 
       assert Trips.by_route(route_id, opts) == response
+    end
+  end
+
+  describe "by_route_and_directions" do
+    test "gets trips on this route in a single direction" do
+      route_id = "39"
+      direction_id = 0
+
+      opts = [
+        get_json_fn: fn "/trips/", [direction_id: ^direction_id, route: ^route_id] ->
+          %JsonApi{
+            data: [
+              %Item{id: route_id, attributes: %{"direction_id" => 0}}
+            ]
+          }
+        end
+      ]
+
+      expected_response = [
+        %Trip{
+          id: route_id,
+          direction_id: direction_id
+        }
+      ]
+
+      assert Trips.by_route_and_directions(route_id, [direction_id], opts) == expected_response
+    end
+
+    test "gets trips on this route in both directions" do
+      route_id = "39"
+
+      opts = [
+        get_json_fn: fn "/trips/", [route: ^route_id] ->
+          %JsonApi{
+            data: [
+              %Item{id: route_id, attributes: %{"direction_id" => 0}},
+              %Item{id: route_id, attributes: %{"direction_id" => 1}}
+            ]
+          }
+        end
+      ]
+
+      expected_response = [
+        %Trip{
+          id: route_id,
+          direction_id: 0
+        },
+        %Trip{
+          id: route_id,
+          direction_id: 1
+        }
+      ]
+
+      assert Trips.by_route_and_directions(route_id, [0, 1], opts) == expected_response
     end
   end
 end
